88ba2da08822b422bde9d2cf1f7a7fa6283f9159
Tim Bavaro <tbavaro@gmail.com>
2018-04-19 23:16:51 -0700

    stop using exceptions; it wasn't a great way to do things like OrValidator anyway, and ES5 compatibility mode breaks it

--- src/Validators.ts
+++ src/Validators.ts
@@ -1,15 +1,25 @@
-export class ValidationError extends Error {
-  public readonly causes?: ValidationError[];
+export type ValidationError = {
+  msg: string;
+  causes?: ValidationError[];
+};
 
-  constructor(msg: string, causes?: ValidationError[]) {
-    super(msg);
-    this.causes = causes;
-  }
+export type ValidationResult = ValidationError | "ok";
+
+export const ValidationOK: ValidationResult = "ok";
+
+function makeError(msg: string, causes?: ValidationError[]): ValidationError {
+  return {
+    msg: msg,
+    causes: causes
+  };
+}
+
+function isError(vr: ValidationResult): vr is ValidationError {
+  return vr !== ValidationOK;
 }
 
 export abstract class Validator {
-  // throws ValidationError if validation fails
-  public abstract validate(input: any): void;
+  public abstract validate(input: any): ValidationResult;
 
   public abstract describe(): { kind: string };
 
@@ -18,7 +28,6 @@ export abstract class Validator {
   }
 }
 
-// TODO it's probably not good to use exceptions for "normal" cases like "or"
 export class OrValidator extends Validator {
   public readonly validators: ReadonlyArray<Validator>;
 
@@ -32,22 +41,20 @@ export class OrValidator extends Validator {
     const succeeded =
       undefined !==
       this.validators.find(validator => {
-        try {
-          validator.validate(input);
+        const result = validator.validate(input);
+        if (isError(result)) {
+          failures.push(result);
+          return false;
+        } else {
           return true;
-        } catch (e) {
-          if (e instanceof ValidationError) {
-            failures.push(e);
-            return false;
-          } else {
-            throw e;
-          }
         }
       });
 
-    if (!succeeded) {
-      throw new ValidationError("OrValidator failed", failures);
-    }
+    return (
+      succeeded
+        ? ValidationOK
+        : makeError("OrValidator failed", failures)
+    );
   }
 
   public describe() {
@@ -67,20 +74,15 @@ export class ArrayValidator extends Validator {
 
   public validate(input: any) {
     if (!(input instanceof Array)) {
-      throw new ValidationError("ArrayValidator failed; not an array");
+      return makeError("ArrayValidator failed; not an array");
     }
 
-    input.forEach((element, index) => {
-      try {
-        this.elementValidator.validate(element)
-      } catch (e) {
-        if (e instanceof ValidationError) {
-          throw new ValidationError(`ArrayValidator failed on element #${index}`, [e]);
-        } else {
-          throw e;
-        }
-      }
+    let result: ValidationResult = ValidationOK;
+    input.find((element, index) => {
+      result = this.elementValidator.validate(element);
+      return isError(result);
     });
+    return result;
   }
 
   public describe() {
@@ -105,25 +107,22 @@ export class ObjectValidator extends Validator {
     // TODO maybe reject if we see any values that aren't supposed to be there
 
     if (typeof input !== "object" || input instanceof Array || input === null) {
-      throw new ValidationError("ObjectValidator failed; not an object");
+      return makeError("ObjectValidator failed; not an object");
     }
 
-    Object.entries(this.propertyValidators).forEach(
+    let result: ValidationResult = ValidationOK;
+    Object.entries(this.propertyValidators).find(
       ([propertyName, validator]) => {
-        try {
-          validator.validate(input[propertyName]);
-        } catch (e) {
-          if (e instanceof ValidationError) {
-            throw new ValidationError(
-              `ObjectValidator failed; property "${propertyName}"`,
-              [e]
-            );
-          } else {
-            throw e;
-          }
+        const myResult = validator.validate(input[propertyName]);
+        if (isError(myResult)) {
+          result = makeError(`ObjectValidator failed; property "${propertyName}"`, [myResult]);
+          return true;
+        } else {
+          return false;
         }
       }
     );
+    return result;
   }
 
   public describe() {
@@ -149,7 +148,9 @@ export class TypeOfValidator extends Validator {
 
   public validate(input: any) {
     if (typeof input !== this.typeOfString) {
-      throw new ValidationError("TypeOfValidator failed");
+      return makeError(`TypeOfValidator (${this.typeOfString}) failed`);
+    } else {
+      return ValidationOK;
     }
   }
 
@@ -170,7 +171,9 @@ export class ExactValueValidator extends Validator {
 
   public validate(input: any) {
     if (input !== this.value) {
-      throw new ValidationError("ExactValueValidator failed");
+      return makeError(`ExactValueValidator (${this.value}) failed`);
+    } else {
+      return ValidationOK;
     }
   }
 
@@ -189,7 +192,10 @@ export const booleanValidator: Validator = new TypeOfValidator("boolean");
 
 export function createValidationFunction<T>(validator: Validator): (input: any) => T {
   return (input => {
-    validator.validate(input);
+    const result = validator.validate(input);
+    if (isError(result)) {
+      throw new Error(result.msg);
+    }
     return input as T;
   });
 }
